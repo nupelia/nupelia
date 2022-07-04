162ad86d90e1469acc69b5a814b202cdcc3825d2
brunomioto <brunomioto97@gmail.com>
2022-07-03 22:10:59 -0300

    datset peld_locais

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Locais de amostragem do PELD.
+#'
+#' Um dataset contendo 36 locais de amostragem já estudados no PELD. Baseado no relatório
+#' @author Edvard, Eder Comunello (Coordenador), Ana Cristina Petry,  Márcia Regina Russo, Anderson Medeiros dos Santos, Renata Ribeiro de Araújo Rocha, Roberto de Albuquerque Leimig
+#' @encoding UTF-8
+#' @format Um data frame com 36 observções e 5 variáveis:
+#' \describe{
+#'   \item{ambiente_tipo}{Tipo do ambiente}
+#'   \item{ambiente_nome}{Nome do ambiente}
+#'   \item{ambiente_codigo}{Codigo do ambiente}
+#'   \item{coordenada}{Coordenadas do ambiente, no padrão 00º'00.00"S, 00º00'00.00"W}
+#'   \item{desc}{A descrição do ambiente}
+#' }
+#' @source \url{http://www.peld.uem.br/Relat2000/2_2_CompBioticoDesLocAmost.PDF}
+"peld_locais"
